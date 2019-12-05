a708d261bfa56725cc2487fae5b589231665cf81
Mavaboan <mavaboan@gmail.com>
2019-12-05 17:24:42 +0100

    Bisektion

--- Bisektion.py
+++ Bisektion.py
@@ -1,23 +1,29 @@
-#Input sat i variabler der skal bestemme hvordan andengradsfunktionen ser ud
-#Samtidig er der også variabler for hvad de 2 intervaller er på hver side a nulpunkte
+# Input sat i variabler der skal bestemme hvordan andengradsfunktionen ser ud
+# Samtidig er der også variabler for hvad de 2 intervaller er på hver side a nulpunkte
 inta = float(input("Hvad er dit interval a? "))
 intb = float(input("Hvad er dit interval b? "))
 a = float(input("Hvad er din a værdi? "))
 b = float(input("Hvad er din b værdi? "))
 c = float(input("Hvad er din c værdi? "))
-potens = int(input("I hvilken potens er funktionen ?"))
+potens = int(input("I hvilken potens er funktionen? "))
 
-#Så mange gange bisektion bliver lavet
-for i in range(15):
-    #Udregningen af bisektion og bliver gemt i variablerne dette vil blive erstattet hver gang den gentager
+loop = True
+# Så mange gange bisektion bliver lavet
+# for i in range(15):
+while loop:
+    # Udregningen af bisektion og bliver gemt i variablerne dette vil blive erstattet hver gang den gentager
     m = (inta + intb) / 2
-    f_m    = ((a * (m ** potens)) + b * m + c)
+    f_m = ((a * (m ** potens)) + b * m + c)
 
-    #Bestemmer om det er enten f(a) eller f(b) der skal udskiftet og printer de nye værdier
+    # Bestemmer om det er enten f(a) eller f(b) der skal udskiftet og printer de nye værdier
     if f_m < 0:
         inta = m
-        print(inta, "..", intb)
+        print(inta, "||", intb)
+        if intb - inta < 0.00001:
+            loop = False
 
     elif f_m > 0:
         intb = m
-        print(inta, "..", intb)
+        print(inta, "||", intb)
+        if intb - inta < 0.00001:
+            loop = False
--- Newtons metode.py
+++ Newtons metode.py
@@ -1,20 +1,20 @@
-#Starts værdi af x
+# Starts værdi af x
 x = float(input("Indtast x-værdi: "))
 
-#Inputs af brugerens valg
+# Inputs af brugerens valg
 a = float(input("Indtast din a-værdi: "))
 
-#a's potens
+# a's potens
 n = int(input("Indtast din n-værdi: "))
 
 b = float(input("Indtast din b-værdi: "))
 
-#b's potens
+# b's potens
 n_1 = int(input("Indtast din anden n-værdi: "))
 
 c = float(input("Indtast din c-værdi: "))
 
-#Differentiering af formelen ud fra brugerens valg af værdier
+# Differentiering af formelen ud fra brugerens valg af værdier
 diff_a = a * n
 
 diff_b = b * n_1
@@ -23,26 +23,26 @@ diff_n = n - 1
 
 diff_n_1 = n_1 - 1
 
-#Formel på diskriminanten
-d=b**2-4*a*c
+# Formel på diskriminanten
+d = b ** 2 - 4 * a * c
 
 if d<0:
     print("Intet nulpunkt")
 
 else:
-#Et loop for newtons metode
+# Et loop for newtons metode
     for i in range(10):
     
-#potenserne løftet op i x-værdien udregner både fra den normale funktion og den differentieret funktion
+# potenserne løftet op i x-værdien udregner både fra den normale funktion og den differentieret funktion
 
         x_diff_n = x ** diff_n
         x_diff_n_1 = x ** diff_n_1
         x_n = x ** n
         x_n_1 = x ** n_1
 
-#Formel for Newtons metoden
+# Formel for Newtons metoden
         N = x - (a * x_n + b * x_n_1 + c) / (diff_a * x_diff_n + diff_b * x_diff_n_1)
 
         print(N)
-#x Sættes til ans af N
+# x Sættes til ans af N
         x = N
